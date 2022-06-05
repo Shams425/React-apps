81deef2d1365989d76954470c473db0c42b31f64
Shams425 <alexdavid486@gmail.com>
2022-06-05 09:49:06 +0200

    adding the cursors

--- Web-Design-Tools/src/components/Tools/cursor/Cursor.jsx
+++ Web-Design-Tools/src/components/Tools/cursor/Cursor.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import "./cursor.css";
 
 export default function Cursor() {
   return (
     <div className="wrapper">
       <div className="cursorContainer">
-        <div className="cursor"></div>
-        <div className="cursor"></div>
-        <div className="cursor"></div>
-        <div className="cursor"></div>
-        <div className="cursor"></div>
-        <div className="cursor"></div>
-        <div className="cursor"></div>
-        <div className="cursor"></div>
+        <div className="cursor eg1"></div>
+        <div className="cursor eg2"></div>
+        <div className="cursor eg3"></div>
+        <div className="cursor eg4"></div>
+        <div className="cursor eg5"></div>
+        <div className="cursor eg6"></div>
+        <div className="cursor eg7"></div>
+        <div className="cursor eg8"></div>
+        <div className="cursor eg9"></div>
+        <div className="cursor eg10"></div>
+        <div className="cursor eg11"></div>
+        <div className="cursor eg12"></div>
+        <div className="cursor eg13"></div>
+        <div className="cursor eg14"></div>
+        <div className="cursor eg15"></div>
+        <div className="cursor eg16"></div>
+        <div className="cursor eg17"></div>
+        <div className="cursor eg18"></div>
+        <div className="cursor eg19"></div>
+        <div className="cursor eg20"></div>
+        <div className="cursor eg21"></div>
+        <div className="cursor eg22"></div>
       </div>
     </div>
   );
