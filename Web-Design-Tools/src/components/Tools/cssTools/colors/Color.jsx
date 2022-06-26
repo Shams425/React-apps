91a8af1a277e048094442c78f86ec8d5df352ce6
Shams425 <alexdavid486@gmail.com>
2022-06-26 22:20:19 +0200

    adding a button to show/hide the picker

--- Web-Design-Tools/src/components/Tools/cssTools/colors/Color.jsx
+++ Web-Design-Tools/src/components/Tools/cssTools/colors/Color.jsx
@@ -4,9 +4,27 @@ import "./color.css";
 
 export default function Color() {
   const [color, setColor] = useState("fff");
+  const [showColorPicker, setShowColorPicker] = useState(false);
   return (
     <div className="wrapper">
-      <ChromePicker color={color} onChange={(e) => setColor(e.hex)} />
+      <button
+        onClick={() => {
+          console.log(showColorPicker);
+          !showColorPicker
+            ? setShowColorPicker(true)
+            : setShowColorPicker(false);
+        }}
+      >
+        {showColorPicker ? "hidePicker" : "showPicker"}
+      </button>
+      {showColorPicker && (
+        <ChromePicker
+          color={color}
+          onChange={(e) => setColor(e.hex)}
+          className="mt-3"
+        />
+      )}
+      <h3 className="text-center">{color}</h3>
     </div>
   );
 }
